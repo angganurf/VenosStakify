8cc78d1c6d3a1b727cfb0bf7e639ad6f6339c6e8
angganurf <angganurfaizal234@gmail.com>
2023-10-17 18:44:27 +0700

    Update Contracts 2

--- src/state/lists/hooks.ts
+++ src/state/lists/hooks.ts
@@ -167,7 +167,6 @@ function combineMaps(map1: TokenAddressMap, map2: TokenAddressMap): TokenAddress
   return {
     [ChainId.BSC]: { ...map1[ChainId.BSC], ...map2[ChainId.BSC] },
     [ChainId.BSC_TESTNET]: { ...map1[ChainId.BSC_TESTNET], ...map2[ChainId.BSC_TESTNET] },
-    [ChainId.OPBNB_TESTNET]: { ...map1[ChainId.OPBNB_TESTNET], ...map2[ChainId.OPBNB_TESTNET] },
   }
 }
 
